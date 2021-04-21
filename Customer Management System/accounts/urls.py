1a5ff4e0a86b3a75221825969edf675701b18c07
Nebir Kiron <kironnibir046@gmail.com>
2021-04-22 01:26:36 +0600

    Password reset configuration added

--- Customer Management System/accounts/urls.py
+++ Customer Management System/accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -19,6 +22,24 @@ urlpatterns = [
     path("create_order/<str:pk_create>/",views.create_order, name='create_order'),
     path("update_order/<slug:pk_order>/",views.update_order, name='update_order'),
     path("delete_order/<slug:pk_del>/",views.delete_order, name='delete_order'),
+
+
+    # password reset 
+    path("reset_password/",
+     auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
+     name="reset_password"),
+
+    path("reset_password_sent/", 
+     auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'), 
+     name="password_reset_done"),
+    
+    path("reset/<uidb64>/<token>/",
+     auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'),
+     name="password_reset_confirm"),
+    
+    path("reset_password_complete/",
+     auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
+     name="password_reset_complete"),
 
 ]
 
--- Customer Management System/crm1/settings.py
+++ Customer Management System/crm1/settings.py
@@ -132,3 +132,15 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
+
+
+
+#SMTP Configuration
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kironnibir046@gmail.com'
+EMAIL_HOST_PASSWORD = ' ' #past the key or password app here
