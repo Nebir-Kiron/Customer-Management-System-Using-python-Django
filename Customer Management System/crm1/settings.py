c9ff8d6ecc87bac6c0667a108a7bba2fe72e1f7c
Nebir Kiron <kironnibir046@gmail.com>
2021-04-22 20:10:31 +0600

    postgresql connected

--- Customer Management System/crm1/settings.py
+++ Customer Management System/crm1/settings.py
@@ -78,12 +78,31 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
+#connect Postgresql
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CRM_DB',
+        'USER': 'postgres',
+        'PASSWORD':'kiron12345',
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
+
+
+
+
+
 
 
 # Password validation
